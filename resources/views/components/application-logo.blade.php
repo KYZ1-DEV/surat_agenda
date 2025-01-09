c3735f5c4b531cb3293dd2131ddc85b7afc20be2
KYZ1-DEV <121333674+KYZ1-DEV@users.noreply.github.com>
2025-01-09 15:07:07 +0700

    Update application-logo.blade.php

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<img src="{{ asset('logo_Dis.png') }}" alt="Icon Description" width="800" height="800" {{ $attributes }}>
+<img src="{{ asset('images/logo_Dis.png') }}" alt="Icon Description" width="800" height="800" {{ $attributes }}>
